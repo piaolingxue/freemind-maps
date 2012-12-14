19392a9955becd6b5268a30f36e033a2a0da778a
piaolingxue <piaolingxue305@gmail.com>
2012-12-14 17:33:56 +0800

    update normal distribution

--- normal-distribution.mm
+++ normal-distribution.mm
@@ -1,4 +1,16 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1355455183806" ID="ID_353581002" MODIFIED="1355455183806" TEXT="&#x65b0;&#x5efa;&#x601d;&#x7ef4;&#x5bfc;&#x56fe;"/>
+<node CREATED="1355455183806" ID="ID_353581002" MODIFIED="1355466733227" TEXT="normal distribution">
+<node CREATED="1355467159886" ID="ID_1984042393" MODIFIED="1355467164180" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="img/pt.jpg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
 </map>
