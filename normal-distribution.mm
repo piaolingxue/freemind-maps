d1f2fd5e7cfca6df6ac7a74e48880afa0c0f7206
piaolingxue <piaolingxue305@gmail.com>
2012-12-14 11:22:53 +0800

    add normal distribution map

---
+++ normal-distribution.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1355455183806" ID="ID_353581002" MODIFIED="1355455183806" TEXT="&#x65b0;&#x5efa;&#x601d;&#x7ef4;&#x5bfc;&#x56fe;"/>
+</map>
