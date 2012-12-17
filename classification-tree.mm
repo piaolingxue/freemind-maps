40c5c43615c67dffb144102d2b74ccbccbd36c1d
piaolingxue <piaolingxue305@gmail.com>
2012-12-17 10:58:03 +0800

    initialize classification-tree map.

---
+++ classification-tree.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1355712987081" ID="ID_922935127" MODIFIED="1355713041338" TEXT="classification-tree"/>
+</map>
