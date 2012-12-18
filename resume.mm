a30a7e6f99fcfc10a82f867cd9b2462187812dbc
piaolingxue <piaolingxue305@gmail.com>
2012-12-18 17:27:25 +0800

    add resume map and data-extraction map.

---
+++ resume.mm
@@ -0,0 +1,23 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1355821664857" ID="ID_390867391" MODIFIED="1355821913526" TEXT="resume">
+<node CREATED="1355822076286" ID="ID_1165837059" MODIFIED="1355822080541" POSITION="right" TEXT="&#x5927;&#x89c4;&#x6a21;&#x6570;&#x636e;&#x5206;&#x6790;"/>
+<node CREATED="1355822092486" ID="ID_1523186489" MODIFIED="1355822141421" POSITION="right" TEXT="&#x5927;&#x578b;&#x5206;&#x5e03;&#x5f0f;&#x4e91;&#x8ba1;&#x7b97;&#x5f00;&#x53d1;&#x7ecf;&#x9a8c;"/>
+<node CREATED="1355822113350" ID="ID_1145626659" MODIFIED="1355822116165" POSITION="right" TEXT="&#x641c;&#x7d22;&#x5f15;&#x64ce;"/>
+<node CREATED="1355822237957" ID="ID_1797066321" MODIFIED="1355822243366" POSITION="left" TEXT="&#x4e00;&#x70b9;&#x611f;&#x60f3;">
+<node CREATED="1355822249141" ID="ID_1733097230" MODIFIED="1355822282172">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21457;&#29616;&#24456;&#22810;&#32844;&#20301;&#37117;&#19981;&#36866;&#21512;&#33258;&#24049;,why?
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
