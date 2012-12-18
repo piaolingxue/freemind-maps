55990494149036078de791076079271b067db786
piaolingxue <piaolingxue305@gmail.com>
2012-12-18 22:41:47 +0800

    study HP jobs

--- resume.mm
+++ resume.mm
@@ -1,6 +1,125 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1355821664857" ID="ID_390867391" MODIFIED="1355821913526" TEXT="resume">
+<node CREATED="1355821664857" ID="ID_390867391" MODIFIED="1355831904400" TEXT="resume">
+<node CREATED="1355831980680" ID="ID_382577444" MODIFIED="1355831985552" POSITION="right" TEXT="&#x4e2a;&#x4eba;&#x7b80;&#x5386;">
+<node CREATED="1355832378566" ID="ID_972063636" MODIFIED="1355832382510" TEXT="English">
+<node CREATED="1355831987480" FOLDED="true" ID="ID_335649766" MODIFIED="1355832277517" TEXT="&#x82f1;&#x6587;&#x81ea;&#x6211;&#x4ecb;&#x7ecd;">
+<node CREATED="1355832195007" ID="ID_910302557" MODIFIED="1355832201143">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Good morning !
+    </p>
+    <p>
+      It is really my honor to have this opportunity for an interview,
+    </p>
+    <p>
+      I hope i can make a good performance today. I'm confident that I can succeed.
+    </p>
+    <p>
+      Now i will introduce myself briefly
+    </p>
+    <p>
+      I am 26 years old,born in shandong province .
+    </p>
+    <p>
+      I was graduated from qingdao university. my major is electronic.and i got my bachelor degree after my graduation in the year of 2003.
+    </p>
+    <p>
+      I spend most of my time on study,i have passed CET4/6 . and i have acquired basic knowledge of my major during my school time.
+    </p>
+    <p>
+      In July 2003, I began work for a small private company as a technical support engineer in QingDao city.Because I'm capable of more responsibilities, so I decided to change my job.
+    </p>
+    <p>
+      And in August 2004,I left QingDao to BeiJing and worked for a foreign enterprise as a automation software test engineer.Because I want to change my working environment, I'd like to find a job which is more challenging. Morover Motorola is a global company, so I feel I can gain the most from working in this kind of company ennvironment. That is the reason why I come here to compete for this position.
+    </p>
+    <p>
+      I think I'm a good team player and I'm a person of great honesty to others. Also I am able to work under great pressure.
+    </p>
+    <p>
+      That&#8217;s all. Thank you for giving me the chance.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1355832230679" ID="ID_97660116" MODIFIED="1355832234653">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Good morning. I am glad to be here for this interview. First let me introduce myself. My name is ***, 24. I come from ******,the capital of *******Province. I graduated from the ******* department of *****University in July ,2001.In the past two years I have been preparing for the postgraduate examination while I have been teaching *****in NO.****middle School and I was a head-teacher of a class in junior grade two.Now all my hard work has got a result since I have a chance to be interview by you . I am open-minded ,quick in thought and very fond of history.In my spare time,I have broad interests like many other youngsters.I like reading books, especially those about *******.Frequently I exchange with other people by making comments in the forum on line.In addition ,during my college years,I was once a Net-bar technician.So, I have a comparatively good command of network application.I am able to operate the computer well.I am skillful in searching for information in Internet.I am a football fan for years.Italian team is my favorite.Anyway,I feel great pity for our country&#8217;s team. I always believe that one will easily lag behind unless he keeps on learning .Of course, if I am given a chance to study ****** in this famous University,I will stare no effort to master a good command of advance ******.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1355832244655" ID="ID_196625309" MODIFIED="1355832252908">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Good afternoon .I am of great hornor to stand here and introduce myself to you .First of all ,my english name is ...and my chinese name is ..If you are going to have a job interview ,you must say much things which can show your willness to this job ,such as ,it is my long cherished dream to be ...and I am eager to get an opportunity to do...and then give some examples which can give evidence to .then you can say something about your hobbies .and it is best that the hobbies have something to do with the job.
+    </p>
+    <p>
+      What is more important is do not forget to communicate with the interviewee,keeping a smile and keeping your talks interesting and funny can contribute to the success.
+    </p>
+    <p>
+      I hope you will give them a wonderful speech .Good luck to you !
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1355832261751" ID="ID_1777246056" MODIFIED="1355832268753">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      good morning, my name is jack, it is really a great honor to have this opportunity for a interview, i would like to answer whatever you may raise, and i hope i can make a good performance today, eventually enroll in this prestigious university in september. now i will introduce myself briefly,i am 21 years old,born in heilongjiang province ,northeast of china,and i am curruently a senior student at beijing XX uni.my major is packaging engineering.and i will receive my bachelor degree after my graduation in june.in the past 4 years,i spend most of my time on study,i have passed CET4/6 with an ease. and i have acquired basic knowledge of packaging and publishing both in theory and in practice. besides, i have attend several packaging exhibition hold in Beijing, this is our advantage study here, i have taken a tour to some big factory and company. through these i have a deeply understanding of domestic packaging industry. compared to developed countries such as us, unfortunately, although we have made extraordinary progress since 1978,our packaging industry are still underdeveloped, mess, unstable, the situation of employees in this field are awkard. but i have full confidence in a bright future if only our economy can keep the growth pace still. i guess you maybe interested in the reason itch to law, and what is my plan during graduate study life, i would like to tell you that pursue law is one of my lifelong goal,i like my major packaging and i won't give up,if i can pursue my master degree here i will combine law with my former education. i will work hard in thesefields ,patent ,trademark, copyright, on the base of my years study in department of p&amp;p, my character? i cannot describe it well, but i know i am optimistic and confident. sometimes i prefer to stay alone, reading, listening to music, but i am not lonely, i like to chat with my classmates, almost talk everything ,my favorite pastime is valleyball,playing cards or surf online. through college life,i learn how to balance between study and entertainment. by the way, i was a actor of our amazing drama club. i had a few glorious memory on stage. that is my pride.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1355832386806" FOLDED="true" ID="ID_592547319" MODIFIED="1355832724932" TEXT="&#x5176;&#x4ed6;&#x95ee;&#x9898;">
+<node CREATED="1355832395317" ID="ID_706034880" MODIFIED="1355832397374" TEXT="What is your greatest strength?&#xff08;&#x4f60;&#x6700;&#x7a81;&#x51fa;&#x7684;&#x4f18;&#x70b9;&#x662f;&#x4ec0;&#x4e48;&#xff1f;&#xff09;">
+<node CREATED="1355832410725" ID="ID_314957654" MODIFIED="1355832436542">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#36825;&#26159;&#24456;&#22810;&#38754;&#35797;&#32771;&#23448;&#21916;&#27426;&#38382;&#30340;&#19968;&#20010;&#38382;&#39064;&#65292;&#36825;&#26159;&#20320;&#23637;&#31034;&#33258;&#24049;&#30340;&#26368;&#20339;&#26426;&#20250;&#65292;&#19981;&#35201;&#21561;&#22040;&#33258;&#24049;&#25110;&#36807;&#20110;&#33258;&#36127;&#65292;&#20294;&#35201;&#35753;&#38599;&#20027;&#30693;&#36947;&#20320;&#30456;&#20449;&#33258;&#24049;&#65292;&#20320;&#30693;&#36947;&#33258;&#24049;&#30340;&#20248;&#28857;&#12290;&#22914;&#21487;&#31572;&#65306;&#8220;&#25105;&#35748;&#20026;&#25105;&#26368;&#22823;&#30340;&#20248;&#28857;&#26159;&#33021;&#22815;&#25191;&#30528;&#22320;&#23613;&#21147;&#25226;&#20107;&#24773;&#21150;&#22909;&#12290;&#24403;&#20570;&#23436;&#19968;&#20214;&#24037;&#20316;&#32780;&#20854;&#25104;&#26524;&#21448;&#27491;&#21512;&#25105;&#30340;&#39044;&#24819;&#26102;&#65292;&#25105;&#20250;&#26377;&#19968;&#31181;&#30495;&#27491;&#30340;&#25104;&#23601;&#24863;&#12290;&#25105;&#32473;&#33258;&#24049;&#23450;&#20102;&#19968;&#20123;&#39640;&#30446;&#26631;&#12290;&#27604;&#22914;&#35828;&#65292;&#25105;&#35201;&#25104;&#20026;&#20986;&#33394;&#30340;&#27605;&#19994;&#29983;&#12290;&#23613;&#31649;&#22312;&#22823;&#23398;&#19968;&#24180;&#32423;&#26102;&#25105;&#21551;&#21160;&#24930;&#20102;&#20123;&#65292;&#20294;&#26368;&#32456;&#25105;&#20197;&#20248;&#31561;&#35770;&#25991;&#23436;&#25104;&#20102;&#23398;&#19994;&#12290;&#8221;
+    </p>
+    <p>
+      I feel that my strongest asset is my ability to stick to things to get them done. I feel a real sense of accomplishment when I finish a job and it turns out just as I&#8217;d planned. I&#8217;ve set some high goals for myself. For example, I want to graduate with highest distinction. And even though I had a slow start in my freshman year, I made up for it by doing an honor&#8217;s thesis.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1355832447893" ID="ID_971815853" MODIFIED="1355832450790" TEXT="What is your greatest weakness?&#xff08;&#x4f60;&#x6700;&#x5927;&#x7684;&#x5f31;&#x70b9;&#x662f;&#x4ec0;&#x4e48;&#xff1f;&#xff09; ">
+<node CREATED="1355832458781" ID="ID_1456659777" MODIFIED="1355832462642" TEXT="&#x4f60;&#x4e0d;&#x5e94;&#x8be5;&#x8bf4;&#x4f60;&#x6ca1;&#x6709;&#x4efb;&#x4f55;&#x5f31;&#x70b9;&#xff0c;&#x4ee5;&#x6b64;&#x6765;&#x56de;&#x907f;&#x8fd9;&#x4e2a;&#x95ee;&#x9898;&#x3002;&#x6bcf;&#x4e2a;&#x4eba;&#x90fd;&#x6709;&#x5f31;&#x70b9;&#xff0c;&#x6700;&#x4f73;&#x7b56;&#x7565;&#x662f;&#x627f;&#x8ba4;&#x4f60;&#x7684;&#x5f31;&#x70b9;&#xff0c;&#x4f46;&#x540c;&#x65f6;&#x8868;&#x660e;&#x4f60;&#x5728;&#x4e88;&#x4ee5;&#x6539;&#x8fdb;&#xff0c;&#x5e76;&#x6709;&#x514b;&#x670d;&#x5f31;&#x70b9;&#x7684;&#x8ba1;&#x5212;&#x3002;&#x53ef;&#x80fd;&#x7684;&#x8bdd;&#xff0c;&#x4f60;&#x53ef;&#x8bf4;&#x51fa;&#x4e00;&#x9879;&#x53ef;&#x80fd;&#x4f1a;&#x7ed9;&#x516c;&#x53f8;&#x5e26;&#x6765;&#x597d;&#x5904;&#x7684;&#x5f31;&#x70b9;&#xff0c;&#x5982;&#x53ef;&#x8bf4;&#xff1a;&#x201c;&#x6211;&#x662f;&#x4e00;&#x4e2a;&#x5b8c;&#x7f8e;&#x4e3b;&#x4e49;&#x8005;&#x3002;&#x5de5;&#x4f5c;&#x505a;&#x5f97;&#x4e0d;&#x6f02;&#x4eae;&#xff0c;&#x6211;&#x662f;&#x4e0d;&#x4f1a;&#x6492;&#x624b;&#x7684;&#x3002;&#x201d; I&apos;m such a perfectionist that I will not stop until a job is well done. "/>
+</node>
+<node CREATED="1355832483141" ID="ID_1160320039" MODIFIED="1355832484598" TEXT="what is your major&#xff1f;">
+<node CREATED="1355832501093" ID="ID_456132549" MODIFIED="1355832578165" TEXT="My major is Computer Science And Technology,I like it very much!"/>
+</node>
+<node CREATED="1355832667915" ID="ID_1178163326" MODIFIED="1355832669181" TEXT="Which university are you attending">
+<node CREATED="1355832670796" ID="ID_1416826042" MODIFIED="1355832685941" TEXT="Suzhou University Of Science And Technology"/>
+</node>
+</node>
+</node>
+</node>
 <node CREATED="1355822076286" ID="ID_1165837059" MODIFIED="1355822080541" POSITION="right" TEXT="&#x5927;&#x89c4;&#x6a21;&#x6570;&#x636e;&#x5206;&#x6790;"/>
 <node CREATED="1355822092486" ID="ID_1523186489" MODIFIED="1355822141421" POSITION="right" TEXT="&#x5927;&#x578b;&#x5206;&#x5e03;&#x5f0f;&#x4e91;&#x8ba1;&#x7b97;&#x5f00;&#x53d1;&#x7ecf;&#x9a8c;"/>
 <node CREATED="1355822113350" ID="ID_1145626659" MODIFIED="1355822116165" POSITION="right" TEXT="&#x641c;&#x7d22;&#x5f15;&#x64ce;"/>
@@ -15,9 +134,26 @@
       &#21457;&#29616;&#24456;&#22810;&#32844;&#20301;&#37117;&#19981;&#36866;&#21512;&#33258;&#24049;,why?
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
+</node>
+<node CREATED="1355831910811" ID="ID_775887864" MODIFIED="1355831923176" POSITION="left" TEXT="HP&#x62db;&#x8058;&#x7684;&#x5de5;&#x4f5c;">
+<node CREATED="1355831931529" ID="ID_1229466508" MODIFIED="1355831933098" TEXT="SD"/>
+<node CREATED="1355831933737" ID="ID_364427723" MODIFIED="1355831936142" TEXT="BW"/>
+<node CREATED="1355831938810" ID="ID_44020806" MODIFIED="1355831939401" TEXT="MM"/>
+<node CREATED="1355831943105" ID="ID_1478275292" MODIFIED="1355831944832" TEXT="ABAP"/>
+<node CREATED="1355832012872" ID="ID_1495927512" MODIFIED="1355832015063" TEXT="WWIS"/>
+<node CREATED="1355832029952" ID="ID_1817608924" MODIFIED="1355832031825" TEXT="MS BI"/>
+<node CREATED="1355832034560" ID="ID_676257901" MODIFIED="1355832035432" TEXT="BO"/>
+<node CREATED="1355832037112" ID="ID_1633054536" MODIFIED="1355832038015" TEXT="BI"/>
+<node CREATED="1355832040064" ID="ID_697382660" MODIFIED="1355832041368" TEXT="Java"/>
+<node CREATED="1355832067272" ID="ID_1026460105" MODIFIED="1355832068313" TEXT=".net"/>
+<node CREATED="1355832071720" ID="ID_891523003" MODIFIED="1355832074464" TEXT="Linux C++"/>
+<node CREATED="1355832076496" ID="ID_1730698942" MODIFIED="1355832078967" TEXT="Linux Script"/>
+<node CREATED="1355832080960" ID="ID_1393593390" MODIFIED="1355832083112" TEXT="Linux Admin"/>
+<node CREATED="1355832085320" ID="ID_1685643649" MODIFIED="1355832091915" TEXT="ITI Perl"/>
+<node CREATED="1355832093888" ID="ID_334447535" MODIFIED="1355832095840" TEXT="OLAP"/>
+<node CREATED="1355832103831" ID="ID_1740108161" MODIFIED="1355832132656" TEXT="Agile Project Management"/>
 </node>
 </node>
 </map>
