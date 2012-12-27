3f7378f61a061b64ae55253fb483139daeeb4063
piaolingxue <piaolingxue305@gmail.com>
2012-12-27 23:01:35 +0800

    add shell mm.

---
+++ shell.mm
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_657852804" 
+	TEXT="shell">
+<node ID="ID_307071467" POSITION="right" 
+	TEXT="1"/>
+<node FOLDED="true" ID="ID_182967750" POSITION="left" 
+	TEXT="ln">
+<node FOLDED="true" ID="ID_666879720" 
+	TEXT="ln -L a ~/a">
+<node ID="ID_1808551374" 
+	TEXT="logic make hard links to symbolic link references"/>
+</node>
+</node>
+</node>
+</map>
