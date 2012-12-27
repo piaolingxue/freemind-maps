a91942ad837d91b44d8af8f2464b3da77bf9984a
piaolingxue <piaolingxue305@gmail.com>
2012-12-27 23:01:51 +0800

    add emacs mm.

---
+++ emacs.mm
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_924539993" 
+	TEXT="emacs">
+<node ID="ID_1063448085" POSITION="right" 
+	TEXT=""/>
+</node>
+</map>
