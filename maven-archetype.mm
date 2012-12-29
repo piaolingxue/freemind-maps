749ca5d8d166e338840915197d7359e982f03a20
piaolingxue <piaolingxue305@gmail.com>
2012-12-29 10:09:24 +0800

    add maven and maven-archetype map.

---
+++ maven-archetype.mm
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1135594891" LINK="maven.mm" 
+	TEXT="archetype"/>
+</map>
---
+++ maven.mm
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1566905636" 
+	TEXT="mvn">
+<node ID="ID_1799284488" LINK="maven-archetype.mm" POSITION="right" 
+	TEXT="archetype"/>
+</node>
+</map>
