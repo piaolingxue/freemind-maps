40139962722cd057b7f70d2a103b9dd4e0d89f84
piaolingxue <piaolingxue305@gmail.com>
2012-12-29 18:27:22 +0800

    add maven-archetype and hadoop map.

--- maven-archetype.mm
+++ maven-archetype.mm
@@ -4,5 +4,39 @@
 <!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
 <!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
 <node ID="ID_1135594891" LINK="maven.mm" 
-	TEXT="archetype"/>
+	TEXT="archetype">
+<node ID="ID_236417260" POSITION="right" 
+	TEXT="maven&apos;s template project"/>
+<node FOLDED="true" ID="ID_1141540209" POSITION="right" 
+	TEXT="相关资料">
+<icon BUILTIN="gohome"/>
+<node ID="ID_452785480" LINK="http://maven.apache.org/guides/introduction/introduction-to-archetypes.html" 
+	TEXT="archetype 介绍"/>
+</node>
+<node FOLDED="true" ID="ID_1820125900" POSITION="right" 
+	TEXT="some maven archetype example">
+<node ID="ID_1547112327" 
+	TEXT="maven-archetype-archetype"/>
+<node ID="ID_1798066037" 
+	TEXT="maven-archetype-j2ee-simple"/>
+<node ID="ID_924725014" 
+	TEXT="maven-archetype-plugin"/>
+<node ID="ID_1932696751" 
+	TEXT="maven-archetype-quickstart"/>
+<node ID="ID_1293387427" 
+	TEXT="maven-archetype-site"/>
+</node>
+<node ID="ID_12890459" POSITION="right" 
+	TEXT="FAQ">
+<icon BUILTIN="clanbomber"/>
+</node>
+<node FOLDED="true" ID="ID_1172243492" POSITION="left" 
+	TEXT="guide to create archetype">
+<icon BUILTIN="wizard"/>
+<node ID="ID_161032635" 
+	TEXT=" Create a new project and pom.xml for the archetype artifact">
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+</node>
 </map>
