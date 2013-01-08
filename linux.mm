ab0d6dcb338b5e7d9d6494dc0c0ae2624420e121
piaolingxue <piaolingxue305@gmail.com>
2013-01-08 10:38:04 +0800

    add linux map.

---
+++ linux.mm
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1540560338" 
+	TEXT="linux">
+<node FOLDED="true" ID="ID_722268172" POSITION="right" 
+	TEXT="add new font">
+<node ID="ID_1998942936" 
+	TEXT="copy the font to /usr/share/fonts/yourdir"/>
+<node ID="ID_1570529305" 
+	TEXT="cd /usr/share/fonts/yourdir"/>
+<node ID="ID_1661802124" 
+	TEXT="sudo mkfontscale"/>
+<node ID="ID_1039967202" 
+	TEXT="sudo mkfontdir"/>
+<node ID="ID_1686863718" 
+	TEXT="sudo fc-cache"/>
+</node>
+</node>
+</map>
