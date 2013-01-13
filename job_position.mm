a9140e64c4ce615a5f850ad03be832d2b56e2108
piaolingxue <piaolingxue305@gmail.com>
2013-01-13 19:12:29 +0800

    add job position map.

---
+++ job_position.mm
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_606024407" 
+	TEXT="position">
+<node FOLDED="true" ID="ID_566913331" POSITION="right" 
+	TEXT="CEO(Chief Executive Officer)">
+<node ID="ID_516420306" 
+	TEXT="首席执行官"/>
+<node ID="ID_1180582603" 
+	TEXT="是一个在一个企业中负责日常经营管理的最高级管理人员"/>
+</node>
+</node>
+</map>
