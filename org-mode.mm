028c3569932d1897cf3dfc66578c9f9dad21d5e2
piaolingxue <piaolingxue305@gmail.com>
2013-01-13 21:47:21 +0800

    org-mode some documents collection.

--- org-mode.mm
+++ org-mode.mm
@@ -4,5 +4,81 @@
 <!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
 <!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
 <node ID="ID_851327566" LINK="emacs.mm" 
-	TEXT="org-mode"/>
+	TEXT="org-mode">
+<node FOLDED="true" ID="ID_1065715227" POSITION="right" 
+	TEXT="Introduction">
+<icon BUILTIN="messagebox_warning"/>
+<node FOLDED="true" ID="ID_1021061045" 
+	TEXT="用途">
+<node ID="ID_780848538" 
+	TEXT="Edit"/>
+<node ID="ID_842416735" 
+	TEXT="Plan"/>
+<node ID="ID_617815193" 
+	TEXT="Clocking"/>
+<node ID="ID_766352449" 
+	TEXT="Agendas"/>
+<node ID="ID_1878344842" 
+	TEXT="Capturing"/>
+<node ID="ID_892255741" 
+	TEXT="Tables"/>
+<node FOLDED="true" ID="ID_347976030" 
+	TEXT="Exporting">
+<node ID="ID_1660506089" 
+	TEXT="html"/>
+<node ID="ID_1000139441" 
+	TEXT="latex"/>
+<node ID="ID_19626983" 
+	TEXT="odt"/>
+</node>
+<node ID="ID_1470371488" 
+	TEXT="Working With Code"/>
+<node ID="ID_1675369728" 
+	TEXT="With your Mobile phone"/>
+</node>
+<node ID="ID_1853972721" 
+	TEXT="当前版本:7.9.3d"/>
+<node ID="ID_1236306263" 
+	TEXT="git地址:git://orgmode.org/org-mode.git"/>
+<node ID="ID_53278787" 
+	TEXT="emacs 23.33.1 版本自带org-mode(6.33x)"/>
+</node>
+<node ID="ID_1752702264" POSITION="right" 
+	TEXT="Commands">
+<icon BUILTIN="yes"/>
+</node>
+<node FOLDED="true" ID="ID_138400939" POSITION="right" 
+	TEXT="相关资料">
+<icon BUILTIN="gohome"/>
+<node ID="ID_878643682" LINK="http://orgmode.org/" 
+	TEXT="Official URL"/>
+<node ID="ID_517156762" LINK="http://www.laihj.net/2009/12/emacs%E7%9A%84org-mode%EF%BC%BB%E5%88%9D%E6%8E%A2%EF%BC%BD/" 
+	TEXT="Emacs 的 org-mode 初探"/>
+<node ID="ID_12007851" LINK="http://orgmode.org/orgcard.pdf" 
+	TEXT="org-mode 命令打印版本"/>
+<node ID="ID_249600597" LINK="http://doc.norang.ca/org-mode.html" 
+	TEXT="Organize Your Life In Plain Text"/>
+<node ID="ID_746261673" LINK="http://www.cnblogs.com/bamanzi/archive/2012/05/12/org-mode-so-powerful.html" 
+	TEXT="为什么说org-mode是神器"/>
+<node ID="ID_112599460" LINK="http://forum.ubuntu.org.cn/viewtopic.php?f=68&amp;t=260068" 
+	TEXT="用org-mode来GTD"/>
+</node>
+<node ID="ID_1237770263" POSITION="right" 
+	TEXT="FAQ">
+<icon BUILTIN="clanbomber"/>
+</node>
+<node ID="ID_1406870648" POSITION="left" 
+	TEXT="Guide Manus and Tutorials">
+<icon BUILTIN="idea"/>
+</node>
+<node FOLDED="true" ID="ID_313980044" POSITION="left" 
+	TEXT="English Words">
+<node ID="ID_134381867" 
+	TEXT="Clocking -&gt; 定时"/>
+<node ID="ID_199852582" 
+	TEXT="agendas -&gt; 会议议程"/>
+<node ID="ID_402884269" 
+	TEXT=""/>
+</node>
+</node>
 </map>
