f73ae094e587030060adf5c0091f63eed770b818
piaolingxue <piaolingxue305@gmail.com>
2013-01-15 17:36:24 +0800

    add emacs gnupot mode map.

---
+++ emacs-gnuplot-mode.mm
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1069983433" LINK="emacs.mm" 
+	TEXT="gnuplot-mode">
+<node ID="ID_489966790" LINK="http://xafs.org/BruceRavel/GnuplotMode" POSITION="right" 
+	TEXT="official url"/>
+<node ID="ID_1605751382" LINK="https://github.com/bruceravel/gnuplot-mode.git" POSITION="right" 
+	TEXT="reps"/>
+</node>
+</map>
