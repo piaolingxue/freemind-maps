59d3e961af5f8349433e884c91a5676cdc7fe0a6
piaolingxue <piaolingxue305@gmail.com>
2013-01-15 17:35:54 +0800

    add ruby study map.

---
+++ ruby.mm
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1759494650" 
+	TEXT="ruby">
+<node FOLDED="true" ID="ID_1778645920" POSITION="right" 
+	TEXT="resources">
+<icon BUILTIN="gohome"/>
+<node ID="ID_1617533822" LINK="http://ruby-china.org/" 
+	TEXT="ruby china"/>
+</node>
+</node>
+</map>
