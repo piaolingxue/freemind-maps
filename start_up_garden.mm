4e06864409ce79458a401244125247f22842203b
piaolingxue <piaolingxue305@gmail.com>
2013-01-15 17:36:08 +0800

    add start_up garden map.

---
+++ start_up_garden.mm
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1646270024" 
+	TEXT="创业">
+<node ID="ID_545057789" POSITION="right" 
+	TEXT="1"/>
+<node FOLDED="true" ID="ID_679183389" POSITION="left" 
+	TEXT="想法">
+<icon BUILTIN="idea"/>
+<node FOLDED="true" ID="ID_433164881" 
+	TEXT="购物解决方案">
+<icon BUILTIN="ksmiletris"/>
+<node FOLDED="true" ID="ID_982634028" 
+	TEXT="示例">
+<node FOLDED="true" ID="ID_989754726" 
+	TEXT="母婴购物解决方案">
+<node ID="ID_1889227230" 
+	TEXT="孕期"/>
+<node ID="ID_819226154" 
+	TEXT="哺乳期"/>
+<node ID="ID_1729994864" 
+	TEXT="不同体质对选购的影响"/>
+</node>
+<node FOLDED="true" ID="ID_1884326143" 
+	TEXT="服装类购物解决方案">
+<node FOLDED="true" ID="ID_828078370" 
+	TEXT="搭配">
+<node ID="ID_1266995345" 
+	TEXT="显瘦"/>
+<node ID="ID_529839308" 
+	TEXT="提臀显高挑"/>
+<node ID="ID_1905528521" 
+	TEXT="小清新"/>
+<node ID="ID_1864282767" 
+	TEXT="潮人"/>
+</node>
+<node ID="ID_915405296" 
+	TEXT=""/>
+</node>
+<node ID="ID_1589544229" 
+	TEXT="美容护肤解决方案"/>
+<node FOLDED="true" ID="ID_1051349572" 
+	TEXT="客厅电器购物解决方案">
+<node ID="ID_1938098751" 
+	TEXT="电器接口"/>
+<node ID="ID_574156919" 
+	TEXT="客厅面积,布局对电器选购的影响"/>
+</node>
+</node>
+<node FOLDED="true" ID="ID_1556004772" 
+	TEXT="设计方案">
+<node ID="ID_416286914" 
+	TEXT="..."/>
+<node ID="ID_1480124780" 
+	TEXT="对每个产品进行打标签"/>
+</node>
+<node ID="ID_250164862" 
+	TEXT="其他解决方案..."/>
+<node ID="ID_153482026" 
+	TEXT="参考资料">
+<icon BUILTIN="gohome"/>
+</node>
+<node ID="ID_330634421" 
+	TEXT="FAQ">
+<icon BUILTIN="clanbomber"/>
+</node>
+</node>
+<node ID="ID_319714354" 
+	TEXT="其他想法..."/>
+</node>
+</node>
+</map>
