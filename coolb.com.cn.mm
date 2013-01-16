3be61c746de6f2597dd4fba44897adba32c00c52
piaolingxue <piaolingxue305@gmail.com>
2013-01-16 20:19:58 +0800

    create website map and coolb.com.cn map.

---
+++ coolb.com.cn.mm
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_637994829" LINK="website.mm" 
+	TEXT="coolb.com.cn">
+<node ID="ID_1672132733" POSITION="right" 
+	TEXT="网站名称:库比, 库B, 酷B, 酷比...."/>
+</node>
+</map>
---
+++ website.mm
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="0.9.0">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1996777552" 
+	TEXT="website">
+<node FOLDED="true" ID="ID_1138135893" POSITION="right" 
+	TEXT="create a website">
+<node FOLDED="true" ID="ID_1812599065" 
+	TEXT="购买域名">
+<node ID="ID_201890598" LINK="http://www.cndns.com/" 
+	TEXT="美橙互联:智能dns收费,只能解析一个ip"/>
+<node ID="ID_1140240111" 
+	TEXT="可以考虑在国外购买域名"/>
+</node>
+<node FOLDED="true" ID="ID_403680772" 
+	TEXT="dns解析">
+<node ID="ID_1157817856">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>介绍:</b>
+    </p>
+    <font size="3">域名解析是把域名指向网站空间IP,让人们通过注册的域名可以方便地访问到网站一种服务。域名解析也叫域名指向、服务器设置、域名配置以及反向IP登记等等。说得简单点就是将好记的域名解析成IP，服务由DNS服务器完成，是把域名解析到一个IP地址，然后在此IP地址的主机上将一个子目录与域名绑定。</font>
+
+    <ol>
+      <li>
+        在域名解析记录上将A记录指向空间ip
+      </li>
+      <li>
+        在空间管理页面,添加该域名
+      </li>
+    </ol>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1901803430" LINK="https://www.dnspod.cn/" 
+	TEXT="dnspod 免费,支持解析多个ip(联通,电信),如果想要更稳定,速度更快可以购买vip"/>
+</node>
+</node>
+<node FOLDED="true" ID="ID_197643100" POSITION="left" 
+	TEXT="my websites">
+<node ID="ID_1055940149" LINK="coolb.com.cn.mm" 
+	TEXT="coolb.com.cn"/>
+</node>
+</node>
+</map>
