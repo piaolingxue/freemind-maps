930535e7ad077fba884c7e514c2f04509b57f21d
piaolingxue <piaolingxue305@gmail.com>
2013-01-20 20:32:34 +0800

    add mind maps about java.

--- java/java.mm
+++ java/java.mm
@@ -4,5 +4,26 @@
 <!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
 <!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
 <node ID="ID_581853415" 
-	TEXT="java"/>
+	TEXT="java">
+<node FOLDED="true" ID="ID_583145166" POSITION="right" 
+	TEXT="基础">
+<node ID="ID_57314945" 
+	TEXT="多线程"/>
+</node>
+<node ID="ID_1160070456" LINK="java设计模式.mm" POSITION="right" 
+	TEXT="设计模式"/>
+<node ID="ID_434836328" POSITION="right" 
+	TEXT=""/>
+<node FOLDED="true" ID="ID_1711381380" POSITION="left" 
+	TEXT="开源框架">
+<node ID="ID_1890911873" 
+	TEXT="struts"/>
+<node ID="ID_139424565" 
+	TEXT="sprint"/>
+<node ID="ID_1248044373" 
+	TEXT="hibernate"/>
+<node ID="ID_1223084037" 
+	TEXT="ibati"/>
+</node>
+</node>
 </map>
